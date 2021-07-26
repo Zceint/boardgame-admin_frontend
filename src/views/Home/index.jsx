a03bec57b995653ac152226623a6b04a306f230f
Zac Huang <zce.int@gmail.com>
2021-07-27 01:08:44 +1000

    added content and routes

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,13 +1,39 @@
 import React from "react";
-
+import { Route, Redirect, Switch } from "react-router-dom";
 import LeftNav from "../../components/LeftNav";
 import Header from "../../components/Header";
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { DRAWER_WIDTH, HEADER_HEIGHT } from "../../config/constant";
+import Dashboard from "../Dashboard";
+import Category from "../Category";
+import Product from "../Product";
+
+const useStyles = makeStyles({
+  content: {
+    backgroundColor: "white",
+    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+    height: `calc(100% - ${HEADER_HEIGHT}px)`,
+    marginTop: HEADER_HEIGHT,
+    marginLeft: DRAWER_WIDTH,
+  },
+});
 
 export default function Home() {
+  const classes = useStyles();
+
   return (
     <>
       <LeftNav />
       <Header />
+      <Box className={classes.content}>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/category" component={Category} />
+          <Route path="/product" component={Product} />
+          <Redirect to="/" />
+        </Switch>
+      </Box>
     </>
   );
 }
